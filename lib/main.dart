e94b8aad77950c76b15bc918a3d2c49f1340a3d9
Jonathan03R <jonathanjesusroquegonzales@gmail.com>
2025-06-20 18:49:12 -0500

    Grafico xd

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:chart_sparkline/chart_sparkline.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,20 +8,48 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    // Cambiado la coma final por punto y coma
+    var data = [0.0, 1.0, 1.5, 2.0, 0.0, 0.0, -0.5, -1.0, -0.5, 0.0, 0.0];
+
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Sesion 12'),
+        ),
         body: Center(
-          child: Text('hello word'),
+          child: Container(
+            width: 400.0,
+            height: 300.0,
+            child: Sparkline(
+              data: data,
+              lineColor: Colors.deepPurple,
+              fillColor: Colors.deepPurple.withOpacity(0.3),
+              lineWidth: 4.0,
+              lineGradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [Colors.deepPurple, Colors.white],
+              ),
+              pointsMode: PointsMode.all,
+              pointSize: 8.0,
+              pointColor: Colors.amber,
+              useCubicSmoothing: true ,
+              cubicSmoothingFactor: 0.2 ,
+              kLine: ['max', 'min', 'first', 'last'],
+              gridLinelabelPrefix: '\$',
+              gridLineLabelPrecision: 3,
+              gridLinesEnable: true,
+
+            ),
+          ),
         ),
       ),
     );
   }
 }
-
